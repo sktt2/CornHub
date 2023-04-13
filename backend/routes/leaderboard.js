8e1b2db1a79ceecb1d8fbc4e018f55db5bcf7c2d
timothy <timothykerong@gmail.com>
2023-04-13 16:43:59 +0800

    Fix sql bug for backend

--- backend/routes/leaderboard.js
+++ backend/routes/leaderboard.js
@@ -11,16 +11,16 @@ router.get("/", (req, res) => {
 
 router.get("/:tagId", (req, res) => {
     id = req.params.tagId
-    db.query(`SELECT * FROM leaderboard WHERE id='${id}'`, function (err, result, fields) {
+    db.query(`SELECT id, score, (SELECT COUNT(*) + 1 FROM leaderboard s2 WHERE s2.score > s1.score) AS position FROM leaderboard s1 WHERE id = '${id}';`, function (err, result, fields) {
         if (err) throw err;
         return res.send(result);
     });
 });
 
 router.post("/", (req, res) => {
-    const { id, score } = req.body
+    var { id, score } = req.body
     id = id.toUpperCase()
-    const query = `INSERT INTO leaderboard (id, score) VALUES (${id}, ${score}) ON DUPLICATE KEY UPDATE portion = ${score};`
+    const query = `INSERT INTO leaderboard (id, score) VALUES ('${id}', '${score}') ON DUPLICATE KEY UPDATE score = ${score};`
     db.query(query, function (err, result, fields) {
         if (err) throw err;
     });
